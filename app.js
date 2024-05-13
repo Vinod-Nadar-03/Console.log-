3f1f14ef1345b5ecf5009bc7ab65939412c7c9b3
Vinod-Nadar-03 <162417445+Vinod-Nadar-03@users.noreply.github.com>
2024-05-13 20:56:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+console.grouplog("Apna Colege");
+
+let pencilPrice = 10;
+let erasorPrice = 5;
+let output = "The total price is: " + (pencilPrice + erasorPrice) + " Rupees.";
+let output = `The total price is: ${pencilPrice+erasorPrice} Rupees.`;
+console.log(output);
+
+//Artithematic Operations
+let a = 10;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
